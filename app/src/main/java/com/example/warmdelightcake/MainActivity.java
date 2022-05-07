6a69b5c2f1acc394f00bfc94f83ed07b57c48754
ayodyaBRanasinghe <it20649494@my.sliit.lk>
2022-05-07 08:41:02 -0700

    updated MainActivity

--- app/src/main/java/com/example/warmdelightcake/MainActivity.java
+++ app/src/main/java/com/example/warmdelightcake/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.warmdelightcake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpToolbar();
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.Managecake:
+
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.Managecake:
+                        Intent intent1 = new Intent(MainActivity.this, signinCake.class);
+
+                        startActivity(intent1);
+                        break;
+
+                    case R.id.Managecake:
+                        Intent intent2 = new Intent(MainActivity.this, updateCake.class);
+
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.Managecake:
+                        Intent intent3 = new Intent(MainActivity.this, deleteCake.class);
+
+                        startActivity(intent3);
+                        break;
+
+                    case R.id.Managecake:
+                        Intent intent4 = new Intent(MainActivity.this, Addcake.class);
+
+                        startActivity(intent4);
+                        break;
+
+                    case R.id.Managecake:
+                        Intent intent5 = new Intent(MainActivity.this, CalculatecakeProfit.class);
+
+                        startActivity(intent4);
+                        break;
+
+
+//Paste your privacy policy link
+
+//                    case  R.id.nav_Policy:{
+//
+//                        Intent browserIntent  = new Intent(Intent.ACTION_VIEW , Uri.parse(""));
+//                        startActivity(browserIntent);
+//
+//                    }
+                    //       break;
+                    case  R.id.nav_share:{
+
+                        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                        String shareSub = "Try now";
+                        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                        sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+
+                    }
+                    break;
+                }
+                return false;
+            }
+        });
+    }
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 }
---
+++ app/src/main/java/com/example/warmdelightcake/updateCake.java
@@ -0,0 +1,4 @@
+package com.example.warmdelightcake;
+
+public class updateCake {
+}
